67c54defd11378ad6a8d9ea1bf03da86682ed596
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2020-12-15 17:54:43 +0100

    added other companies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,40 +34,40 @@ export class AppComponent {
   public lineChartType = 'line';
   public lineChartPlugins = [];
 
-  public stocksList: string[] = [
-    'Biocon',
-    'item1',
-    'item1',
-    'item1',
-    'item1',
-    'item1',
-    'item1',
+  public stocksList: {name: string; code: string}[] = [
+    {name: 'Biocon', code: 'BIOCON'},
+    {name: 'Canara Bank', code: 'CANBK'},
+    {name: 'Ashok Leyland', code: '500477'},
+    {name: 'Tata Steel', code: 'TATASTEEL'},
+    {name: 'Glenmark Pharmaceuticals', code: '532296'},
+    {name: 'HDFC Life', code: '540777'},
+    
   ];
 
   public configUrl = '';
   ngOnInit(): void {}
 
-  public onStockSelection(type: string) {
+  public onStockSelection(type: {name: string; code: string}) {
     // this.lineChartData[0].data = [];
     // this.lineChartData[0].label = type;
     this.lineChartLabels = [];
     this.stockCharts[0].data = [];
-    this.getStockUrl(type.toLocaleUpperCase());
+    this.getStockUrl(type.code);
     if (
       document.activeElement &&
       document.activeElement instanceof HTMLButtonElement
     ) {
       document.activeElement.blur();
     }
-    if (this.lineChartData.filter((entry) => entry.label === type).length > 0) {
+    if (this.lineChartData.filter((entry) => entry.label === type.name).length > 0) {
       this.weekDates.map((entry) => this.lineChartLabels.push(entry.date));
       // this.weekDates.map((x, i) => {return {'stockValue': this.stockCharts[0].data[i]}});
       this.stockCharts[0].data = this.lineChartData.find(
-        (entry) => entry.label === type
+        (entry) => entry.label === type.name
       ).data;
       
       this.stockCharts[0].label = this.lineChartData.find(
-        (entry) => entry.label === type
+        (entry) => entry.label === type.name
       ).label;
       this.weekDates.map((entry, i) => entry.stockValue = this.stockCharts[0].data[i].toString());
       console.log(this.weekDates);
@@ -82,8 +82,8 @@ export class AppComponent {
         );
         this.weekDates.map((entry) => this.lineChartLabels.push(entry.date));
         this.stockCharts[0].data = this.weeklyData;
-        this.stockCharts[0].label = type;
-        this.lineChartData.push({ data: this.weeklyData, label: type });
+        this.stockCharts[0].label = type.name;
+        this.lineChartData.push({ data: this.weeklyData, label: type.name });
         // console.log(this.weekDates);
       });
     }
