a5a427021087c962b997d813de603d9c4f2ba166
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2020-12-15 05:36:38 +0100

    create chart component

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-line-chart',
+  selector: 'stock-line-chart',
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
 
+
+  @Input() lineChartData;
+  @Input() lineChartLabels;
+  @Input() lineChartColors;
+  @Input() lineChartLegend;
+  @Input() lineChartType;
+  @Input() lineChartPlugins;
   constructor() { }
 
   ngOnInit() {
