ffeeda4e00e14aa3b174b4ffeeac71d864e51add
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2020-12-15 05:27:56 +0100

    create tslint json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AttributeDirectiveTut } from './Directives/app.attribute.directive';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, ChartsModule ],
-  declarations: [ 
+  declarations: [	 
     AppComponent,
     DirectiveTut,
-    AttributeDirectiveTut ],
+    AttributeDirectiveTut,
+      LineChartComponent
+   ],
   bootstrap:    [ AppComponent ],
   providers: [ ]
 })
